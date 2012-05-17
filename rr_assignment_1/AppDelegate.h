156b3d5a9b7a09c9dde01c544f79de5515255781
Robert Rivera <robertrivera@MacBook-Pro.local>
2012-05-17 10:06:52 -0500

    Initial Commit

---
+++ rr_assignment_1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rr_assignment_1
+//
+//  Created by Robert Rivera on 5/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rr_assignment_1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  rr_assignment_1
+//
+//  Created by Robert Rivera on 5/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
