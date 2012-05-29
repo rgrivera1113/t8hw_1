4500601febfe53bd77cedae2d0f022df65f8b26b
Rob Rivera <robert.rivera@t8webware.com>
2012-05-29 15:13:03 -0500

    Added basic iPad display (split view controller)

--- rr_assignment_1/ViewController.h
+++ rr_assignment_1/ViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UISplitViewControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *display;
 @property (weak, nonatomic) IBOutlet UILabel *details;
--- rr_assignment_1/ViewController.m
+++ rr_assignment_1/ViewController.m
@@ -26,6 +26,13 @@
 @synthesize brain = _brain;
 @synthesize variables = _variables;
 
+- (void)awakeFromNib  // always try to be the split view's delegate
+{
+    [super awakeFromNib];
+    self.splitViewController.delegate = self;
+}
+
+
 - (Brain*) brain {
     
     if(!_brain)
@@ -155,6 +162,12 @@
         [(GraphVC*) segue.destinationViewController setProgram:[self.brain program]];
         [(GraphVC*) segue.destinationViewController setTitle:[[self.brain class] descriptionOfProgram:[self.brain program]]];
         
+    } else if ([segue.identifier isEqualToString:@"ReplaceGraph"]) {
+        
+        // Set up toolbar and assign data to controller.
+        [segue.destinationViewController setProgram:[self.brain program]];
+        [(GraphVC*) segue.destinationViewController setTitle:[[self.brain class] descriptionOfProgram:[self.brain program]]];
+        
     }
     
     
@@ -164,4 +177,42 @@
     [self setDetails:nil];
     [super viewDidUnload];
 }
+
+// Called when a button should be added to a toolbar for a hidden view controller
+- (void)splitViewController: (UISplitViewController*)svc 
+     willHideViewController:(UIViewController *)aViewController 
+          withBarButtonItem:(UIBarButtonItem*)barButtonItem 
+       forPopoverController: (UIPopoverController*)pc {
+    
+    
+}
+
+// Called when the view is shown again in the split view, invalidating the button and popover controller
+- (void)splitViewController: (UISplitViewController*)svc 
+     willShowViewController:(UIViewController *)aViewController 
+  invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem {
+    
+    
+}
+
+// Called when the view controller is shown in a popover so the delegate can take action like hiding other popovers.
+- (void)splitViewController: (UISplitViewController*)svc 
+          popoverController: (UIPopoverController*)pc 
+  willPresentViewController:(UIViewController *)aViewController {
+    
+    
+    
+}
+
+// Returns YES if a view controller should be hidden by the split view controller in a given orientation.
+// (This method is only called on the leftmost view controller and only discriminates portrait from landscape.)
+- (BOOL)splitViewController: (UISplitViewController*)svc 
+   shouldHideViewController:(UIViewController *)vc 
+              inOrientation:(UIInterfaceOrientation)orientation {
+    
+    return NO;
+    
+}
+
+
 @end
